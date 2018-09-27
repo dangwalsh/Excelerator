4b9fcf1ba8a2ce6af9499c219049b29cc1b2327e
Daniel Walsh <dangwalsh@gmail.com>
2018-09-27 15:56:52 -0400

    final ui

--- Excelerator/Properties/Resources.Designer.cs
+++ Excelerator/Properties/Resources.Designer.cs
@@ -69,5 +69,25 @@ namespace Excelerator.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap hand_left {
+            get {
+                object obj = ResourceManager.GetObject("hand_left", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap hand_right {
+            get {
+                object obj = ResourceManager.GetObject("hand_right", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
